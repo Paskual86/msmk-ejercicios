76d1e9c537b7bd6f7b31f946a641e0c515261133
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2024-03-25 19:32:46 +0100

    Agrego funcionalidad para mostrar imagen cargada

--- TikTokPro/index.js
+++ TikTokPro/index.js
@@ -1,7 +1,13 @@
 const images = document.getElementsByClassName("edit-image");
+const imageSelected = document.getElementById("image-upload");
 
 for (const imgObj of images) {
     imgObj.addEventListener("click", () => {
         window.location.href = "./EditPhoto/Index.html";
     });
 }
+
+imageSelected.addEventListener("change", () => {
+    const imagen = imageSelected.files[0];
+    window.location.href = "./EditPhoto/Index.html";
+});
